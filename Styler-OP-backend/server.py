d35190230c59a3c29d56581eddcc85223d46f8a3
Arpan Mondal <arpanmondal572@gmail.com>
2026-01-21 11:29:04 +0530

    fet: Add image tranform route and schema

--- Styler-OP-backend/server.py
+++ Styler-OP-backend/server.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routes import auth_routes
+from routes import auth_routes , image_routes
 app = FastAPI()
 
 app.include_router(auth_routes.router)
-
+app.include_router(image_routes.router)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
